afb43f641d0b679ffd99d855836468e166ba1d21
didiernyambe <nyambebelle@outlook.com>
2022-10-05 14:32:46 +0100

    Creation d'un jeu en JS

---
+++ js/player.js
@@ -0,0 +1,11 @@
+let player;
+
+
+function Player(classType, health, shield, strength, agility, speed) {
+    this.classType = classType;
+    this.health = health;
+    this.shield = shield;
+    this.strength = strength;
+    this.agility = agility;
+    this.speed = speed;
+}
